ffb1705d2888fa9eff81c9fe9a8b0fb3ddffe5eb
arifwidiyatmiko <arifwidiyatmiko@gmail.com>
2018-03-18 13:37:26 +0700

    re-fixing

--- plugin-paket.php
+++ plugin-paket.php
@@ -13,12 +13,12 @@
  * @package           Plugin_Name
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       NCS Plugin 
+ * Plugin URI:        https://github.com/arifwidiyatmiko/plugin-paket
+ * Description:       Beta Version
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            PT NCS
+ * Author URI:        http://www.ptncs.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-name
